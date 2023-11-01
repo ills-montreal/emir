7ed970f138bef5e144ba1543fe26496ec972c8ba
Icannos <astaligorn@live.com>
2023-11-01 13:41:28 -0400

    summarizer evaluation scripts

--- scripts/summarization_evaluation/evaluate_summarizer.py
+++ scripts/summarization_evaluation/evaluate_summarizer.py
@@ -1,50 +1,55 @@
+import logging
+import argparse
+from pathlib import Path
 from typing import Tuple
 
 import pandas as pd
-import numpy as np
 import torch
-import argparse
+from sentence_transformers import SentenceTransformer
 
-from tqdm import tqdm
-from pathlib import Path
-
-from sentence_transformers import SentenceTransformer, util
 from emir.estimators import KNIFEEstimator, KNIFEArgs
 
+from sys import stdout
+
+
+# logging.basicConfig(stream=stdout, level=logging.)
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="paraphrase-MiniLM-L6-v2")
     parser.add_argument("--summaries", type=Path, default="")
+    parser.add_argument("--output", type=str)
 
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--n_epochs", type=int, default=200)
-    parser.add_argument("--lr", type=float, default=0.1)
-    parser.add_argument("--device", type=str, default="cpu")
-    parser.add_argument("--average", type=str, default="var")
-    parser.add_argument("--cov_diagonal", type=str, default="var")
-    parser.add_argument("--cov_off_diagonal", type=str, default="var")
-    parser.add_argument("--optimize_mu", type=bool, default=False)
-    parser.add_argument("--simu_params", type=list, default=["source_data", "target_data", "method", "optimize_mu"])
+    parser.add_argument("--lr", type=float, default=0.001)
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--average", type=str, default="")
+    parser.add_argument("--cov_diagonal", type=str, default="")
+    parser.add_argument("--cov_off_diagonal", type=str, default="")
+    parser.add_argument("--optimize_mu", default=False, action="store_true")
+
     parser.add_argument("--cond_modes", type=int, default=8)
     parser.add_argument("--marg_modes", type=int, default=8)
-    parser.add_argument("--use_tanh", type=bool, default=True)
+    parser.add_argument("--use_tanh", default=True, action="store_true")
     parser.add_argument("--init_std", type=float, default=0.01)
     parser.add_argument("--ff_residual_connection", type=bool, default=False)
     parser.add_argument("--ff_activation", type=str, default="relu")
-    parser.add_argument("--ff_layer_norm", type=bool, default=True)
+    parser.add_argument("--ff_layer_norm", default=True, action="store_true")
     parser.add_argument("--ff_layers", type=int, default=2)
+
+    # example of use of this script
 
     args = parser.parse_args()
     return args
 
 
-def parse_summaries(path : Path):
-    '''
+def parse_summaries(path: Path):
+    """
     :return: a pandas dataframe with at least the columns 'text' and 'summary'
-    '''
+    """
     # read csv file
 
-    df = pd.read_csv(path)
+    df = pd.read_csv(path, sep=";")
 
     # check if the csv file has the correct columns
     if not all([col in df.columns for col in ["text", "summary"]]):
@@ -52,13 +57,13 @@ def parse_summaries(path : Path):
 
     return df
 
+
 def embedd_sourcetexts_and_summaries(df, model) -> Tuple[torch.Tensor, torch.Tensor]:
-    '''
+    """
     :param df: a pandas dataframe with at least the columns 'text' and 'summary'
     :param model: a sentence transformer model
     :return: a tuple of two torch tensors, one for the text embeddings and one for the summary embeddings
-    '''
-
+    """
 
     # embedd the text and the summary
     text_embeddings = model.encode(df.text.tolist(), convert_to_tensor=True)
@@ -66,22 +71,38 @@ def embedd_sourcetexts_and_summaries(df, model) -> Tuple[torch.Tensor, torch.Ten
 
     return text_embeddings, summary_embeddings
 
-def compute_mi(text_embeddings, summary_embeddings, device, args) -> torch.Tensor:
-    '''
+
+def compute_mi(
+    text_embeddings, summary_embeddings, device, args
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
     :param text_embeddings: a torch tensor of text embeddings
     :param summary_embeddings: a torch tensor of summary embeddings
     :param device: a string specifying the device to use
     :param args: a namespace object with the arguments for the KNIFE estimator
     :return: a torch tensor with the mutual information between the text and the summary
-    '''
+    """
+
+    # clean args from argparse to fit KNIFEArgs: remove the arguments that are not in KNIFEArgs
+    args_dict = vars(args)
+    args_dict = {
+        key: value
+        for key, value in args_dict.items()
+        if key in KNIFEArgs.__annotations__
+    }
 
     # initialize the KNIFE estimator
-    knife_estimator = KNIFEEstimator(KNIFEArgs(**vars(args)), text_embeddings.shape[1], summary_embeddings.shape[1])
+    knife_estimator = KNIFEEstimator(
+        KNIFEArgs(**args_dict), text_embeddings.shape[1], summary_embeddings.shape[1]
+    )
 
     # compute the mutual information
-    mi = knife_estimator.eval(text_embeddings.to(device), summary_embeddings.to(device))
+    mi, marg_ent, cond_ent = knife_estimator.eval(
+        text_embeddings.to(device), summary_embeddings.to(device)
+    )
 
-    return mi
+    return mi, marg_ent, cond_ent
+
 
 def main():
     args = parse_args()
@@ -95,13 +116,37 @@ def main():
     # embedd the text and the summary
     text_embeddings, summary_embeddings = embedd_sourcetexts_and_summaries(df, model)
 
-    # compute the mutual information
-    mi = compute_mi(text_embeddings, summary_embeddings, args.device, args)
+    # compute the mutual information text -> summary
+    mi, marg_ent, cond_ent = compute_mi(
+        text_embeddings, summary_embeddings, args.device, args
+    )
 
-    print(mi)
+    # compute reverse mi: summary -> text
+    mi_rev, marg_ent_rev, cond_ent_rev = compute_mi(
+        summary_embeddings, text_embeddings, args.device, args
+    )
+
+    # make a pandas dataframe, use summaries filename as index
+
+    df = pd.DataFrame(
+        {
+            "I(text -> summary)": [mi],
+            "marg_ent": [marg_ent],
+            "cond_ent": [cond_ent],
+            "I(summary -> text)": [mi_rev],
+            "marg_ent_rev": [marg_ent_rev],
+            "cond_ent_rev": [cond_ent_rev],
+        }
+    )
+    df.index = [args.summaries.stem]
+
+    # save the dataframe
+
+    path = Path(args.output) / f"{args.summaries.stem}_mi.csv"
+    # create the output directory if it does not exist
+    path.parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(path)
 
 
-
-
-
-
+if __name__ == "__main__":
+    main()
--- scripts/summarization_evaluation/generate_summaries.py
+++ scripts/summarization_evaluation/generate_summaries.py
@@ -1,31 +1,195 @@
+import argparse
+from pathlib import Path
+
+import pandas as pd
+from torch.utils.data import DataLoader
+from datasets import Dataset
+from tqdm import tqdm
+import datetime
 
 import transformers
-import torch
-from datasets import load_dataset
 
-import argparse
+GENERATION_CONFIGS = {
+    "top_p_sampling": {
+        "max_new_tokens": 100,
+        "do_sample": True,
+        "top_p": 0.95,
+    }
+}
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", type=str, default="facebook/bart-large-cnn")
     parser.add_argument("--dataset_name", type=str, default="rotten_tomatoes")
+    parser.add_argument("--dataset_path", type=str, default=None)
     parser.add_argument("--decoding_config", type=str, default="top_p_sampling")
+
+    parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--device", type=str, default="cuda")
+
+    parser.add_argument("--output_dir", type=str, default="output")
+
+    # limit the number of samples to generate
+    parser.add_argument("--limit", type=int, default=None)
 
     args = parser.parse_args()
 
     return args
 
+# write command example
+# python EMIR/scripts/summarization_evaluation/generate_summaries.sh.py --model_name facebook/bart-large-cnn --dataset_name rotten_tomatoes --dataset_path data/datasets --decoding_config top_p_sampling --batch_size 16 --device cuda --output_dir output
 
 
-def load_dataset(dataset_name):
+def load_dataset(dataset_name, dataset_path=None) -> Dataset:
     if dataset_name == "rotten_tomatoes":
-        pass
+        path = (
+            Path(dataset_path)
+            / "rotten_tomatoes_critic_reviews.csv"
+        )
+        dataset = pd.read_csv(path)
+
+        # get rotten_tomatoes_link as id, review_content as text and review_score as sentiment
+        new_dataset = pd.DataFrame(
+            {
+                "id": dataset.rotten_tomatoes_link,
+                "text": dataset.review_content,
+                "sentiment": dataset.review_score,
+            }
+        )
+
+        # remove rows with missing values
+        new_dataset = new_dataset.dropna()
+
+        dataset = new_dataset
+
+    elif dataset_name == "peer_read":
+        path = Path(dataset_path) / "peer_read.csv"
+        dataset = pd.read_csv(path)
+
+        # dataframe has columns ['id', 'review_idx',  'title', 'abstract', 'review']
+        # rename review to text
+        dataset = dataset.rename(columns={"review": "text"})
+
+    else:
+        raise ValueError(f"Dataset {dataset_name} not supported.")
+
+    # make a dataset from the dataframe
+    dataset = Dataset.from_pandas(dataset)
+
+    return dataset
+
+
+def evaluate_summarizer(
+    model, tokenizer, dataset: Dataset, decoding_config, batch_size: int
+) -> Dataset:
+    """
+    @param model: The model used to generate the summaries
+    @param tokenizer: The tokenizer used to tokenize the text and the summary
+    @param dataset: A dataset with the text
+    @param decoding_config: Dictoionary with the decoding config
+    @param batch_size: The batch size used to generate the summaries
+    @return: The same dataset with the summaries added
+    """
+    # create a dataset with the text and the summary
+
+    # create a dataloader
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+    # generate summaries
+    summaries = []
+    print("Generating summaries...")
+    for batch in tqdm(dataloader):
+        text = batch["text"]
+        inputs = tokenizer(
+            text,
+            max_length=1024,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt",
+        )
+
+        # move inputs to device
+        inputs = {key: value.to("cuda") for key, value in inputs.items()}
+
+        # generate summaries
+        outputs = model.generate(
+            **inputs,
+            **decoding_config,
+        )
+
+        # decode summaries
+        summaries.extend(
+            [
+                tokenizer.decode(
+                    output,
+                    skip_special_tokens=True,
+                    clean_up_tokenization_spaces=True,
+                )
+                for output in outputs
+            ]
+        )
+
+    # add summaries to the huggingface dataset
+    dataset = dataset.map(lambda example: {"summary": summaries.pop(0)})
+
+    return dataset
+
+
+def sanitize_model_name(model_name: str) -> str:
+    """
+    Sanitize the model name to be used as a folder name.
+    @param model_name: The model name
+    @return: The sanitized model name
+    """
+    return model_name.replace("/", "_")
 
 
 def main():
-    pass
+    args = parse_args()
+
+    # load the model
+    model = transformers.AutoModelForSeq2SeqLM.from_pretrained(args.model_name)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_name)
+
+    # move model to device
+    model = model.to(args.device)
 
 
-if __name__ == '__main__':
+    # load the dataset
+    print("Loading dataset...")
+    dataset = load_dataset(args.dataset_name, args.dataset_path)
+
+    # limit the number of samples
+    if args.limit is not None:
+        _lim = min(args.limit, len(dataset))
+        dataset = dataset.select(range(_lim))
+
+    # generate summaries
+    dataset = evaluate_summarizer(
+        model,
+        tokenizer,
+        dataset,
+        GENERATION_CONFIGS[args.decoding_config],
+        args.batch_size,
+    )
+
+    # save the dataset
+    # add unique date in name
+    now = datetime.datetime.now()
+    date = now.strftime("%Y-%m-%d-%H-%M-%S")
+    model_name = sanitize_model_name(args.model_name)
+    output_path = (
+        Path(args.output_dir)
+        / f"{model_name}_{args.dataset_name}_{args.decoding_config}_{date}.csv"
+    )
+
+    # create output dir if it doesn't exist
+    if not output_path.parent.exists():
+        output_path.parent.mkdir(parents=True)
+
+    dataset.to_pandas().to_csv(output_path, index=False, encoding="utf-8", sep=";")
+
+
+if __name__ == "__main__":
     main()
