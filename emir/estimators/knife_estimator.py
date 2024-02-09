e952428446798432226df1d49e374f98ddeabd74
Fransou <philippe.form@gmail.com>
2024-02-09 15:06:44 -0500

    [feat|knife] Add discrete kernels if the target tensor is composed of 1 and 0, and add a much faster Dataloader

--- emir/estimators/knife_estimator.py
+++ emir/estimators/knife_estimator.py
@@ -71,7 +71,13 @@ class KNIFEEstimator:
         """
 
         # Create model for MI estimation
-        self.knife = KNIFE(self.args, self.x_dim, self.y_dim).to(self.args.device)
+        if (y == 0).logical_or(y == 1).all():
+            kernel_type = "discrete"
+        else:
+            kernel_type = "gaussian"
+        self.knife = KNIFE(
+            self.args, self.x_dim, self.y_dim, kernel_type=kernel_type
+        ).to(self.args.device)
 
         # Fit the model
         self.fit_estimator(x, y, record_loss=record_loss)
@@ -92,30 +98,22 @@ class KNIFEEstimator:
 
         optimizer = torch.optim.SGD(self.knife.parameters(), lr=self.args.lr)
 
-        train_loader = torch.utils.data.DataLoader(
-            torch.utils.data.TensorDataset(x, y), batch_size=self.args.batch_size
+        train_loader = FastTensorDataLoader(x, y, batch_size=self.args.batch_size,
         )
 
-        losses = []
-
-        for epoch in trange(self.args.n_epochs, position=-1, desc="Knife training", leave=False):
+        for epoch in trange(
+            self.args.n_epochs, position=-1, desc="Knife training", leave=False
+        ):
             epoch_loss = []
             for x_batch, y_batch in train_loader:
-                # move data to device
-                x_batch = x_batch.to(self.args.device)
-                y_batch = y_batch.to(self.args.device)
-
                 optimizer.zero_grad()
                 loss = self.knife.learning_loss(x_batch, y_batch)
                 loss.backward()
                 optimizer.step()
-
-                losses.append(loss.item())
-
                 epoch_loss.append(loss.item())
 
             self.recorded_loss.append(sum(epoch_loss) / len(epoch_loss))
-            logger.info("Epoch %d: loss = %f", epoch, loss.item())
+            logger.info("Epoch %d: loss = %f", epoch, self.recorded_loss[-1])
 
             if self.args.stopping_criterion == "early_stopping":
                 if (
@@ -126,4 +124,50 @@ class KNIFEEstimator:
                     logger.info("Reached early stopping criterion")
                     break
 
-        return losses
+
+class FastTensorDataLoader:
+    """
+    A DataLoader-like object for a set of tensors that can be much faster than
+    TensorDataset + DataLoader because dataloader grabs individual indices of
+    the dataset and calls cat (slow).
+    Source: https://discuss.pytorch.org/t/dataloader-much-slower-than-manual-batching/27014/6
+    """
+    def __init__(self, *tensors, batch_size=32, shuffle=False):
+        """
+        Initialize a FastTensorDataLoader.
+
+        :param *tensors: tensors to store. Must have the same length @ dim 0.
+        :param batch_size: batch size to load.
+        :param shuffle: if True, shuffle the data *in-place* whenever an
+            iterator is created out of this object.
+
+        :returns: A FastTensorDataLoader.
+        """
+        assert all(t.shape[0] == tensors[0].shape[0] for t in tensors)
+        self.tensors = tensors
+
+        self.dataset_len = self.tensors[0].shape[0]
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+
+        # Calculate # batches
+        n_batches, remainder = divmod(self.dataset_len, self.batch_size)
+        if remainder > 0:
+            n_batches += 1
+        self.n_batches = n_batches
+    def __iter__(self):
+        if self.shuffle:
+            r = torch.randperm(self.dataset_len)
+            self.tensors = [t[r] for t in self.tensors]
+        self.i = 0
+        return self
+
+    def __next__(self):
+        if self.i >= self.dataset_len:
+            raise StopIteration
+        batch = tuple(t[self.i:self.i+self.batch_size] for t in self.tensors)
+        self.i += self.batch_size
+        return batch
+
+    def __len__(self):
+        return self.n_batches
