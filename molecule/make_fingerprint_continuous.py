b1df4d2534edef195b19ba659faf4c850dc7cff1
Fransou <philippe.form@gmail.com>
2024-02-21 16:57:42 -0500

    [emir|feat] Add new distance (Tanimoto) to the Kmeans possible distances

---
+++ molecule/make_fingerprint_continuous.py
@@ -0,0 +1,103 @@
+"""Script transforming binary fingerprints into continuous ones using the MDS algorithm. (Can be long on large datasets)"""
+
+import argparse
+import json
+import numpy as np
+from utils import get_features
+from sklearn.manifold import MDS
+
+from tqdm import tqdm
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="Script transforming binary fingerprints into continuous ones using the MDS algorithm. (Can be long on large datasets)"
+    )
+
+    parser.add_argument(
+        "--descriptors",
+        type=str,
+        nargs="+",
+        default=[
+            #"default",
+            "cats",
+            "gobbi",
+            "pmapper",
+            "cats/3D",
+            "gobbi/3D",
+            "pmapper/3D",
+        ],
+        help="List of descriptors to compare",
+    )
+
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="ClinTox",
+        help="Dataset to use",
+    )
+
+    parser.add_argument(
+        "--out-dir",
+        type=str,
+        default="data",
+        help="Output file",
+    )
+
+    parser.add_argument("--fp-length", type=int, default=1024)
+    parser.add_argument("--out-dim", type=int, default=64)
+
+    return parser
+
+
+def main(args):
+    with open(f"data/{args.dataset}/smiles.json", "r") as f:
+        smiles = json.load(f)
+    for desc in tqdm(args.descriptors):
+        print(f"Processing {desc}...")
+        print(f"Loading {args.dataset}...")
+        descriptors_embedding = get_features(
+            smiles=smiles,
+            name=desc,
+            length=args.fp_length,
+            feature_type="descriptor",
+            device="cpu",
+            dataset=args.dataset,
+        )
+        if (
+                len(descriptors_embedding.unique()) < 1500
+                and not (descriptors_embedding == 0)
+                .logical_or(descriptors_embedding == 1)
+                .all()
+        ):
+            # setting to 1 all elements different from 0
+            descriptors_embedding = (descriptors_embedding != 0).float()
+
+        if (descriptors_embedding == 0).logical_or(descriptors_embedding == 1).all():
+            print(f"Computing Tanimoto similarity...")
+            intersection = descriptors_embedding @ descriptors_embedding.T
+            union = (
+                descriptors_embedding.sum(1)[:, None]
+                + descriptors_embedding.sum(1)[None, :]
+            )
+            tanimoto = 1-intersection / (union - intersection + 1e-8)
+
+            print(f"Computing continuous fingerprints...")
+            mds = MDS(
+                n_components=args.out_dim,
+                dissimilarity="precomputed",
+                n_init=1,
+                verbose=0,
+                n_jobs=1,
+            )
+            continuous_fingerprints = mds.fit_transform(tanimoto)
+            print(f"Saving continuous fingerprints...")
+            np.save(
+                f"""{args.out_dir}/{args.dataset}/{desc.replace("/", "_")}_{args.fp_length}_mds_{args.out_dim}.npy""",
+                continuous_fingerprints,
+            )
+        print("Done.")
+
+if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+    main(args)
