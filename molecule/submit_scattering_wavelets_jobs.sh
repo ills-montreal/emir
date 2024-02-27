ac8d358d9b07949879a9042b825cc4d18404efa4
Fransou <philippe.form@gmail.com>
2024-02-27 11:12:10 -0500

    [mol|ZINC-scatt] modify scripts hardcoded values

--- molecule/submit_scattering_wavelets_jobs.sh
+++ molecule/submit_scattering_wavelets_jobs.sh
@@ -8,4 +8,5 @@ while [ $start_i -lt $max_i ]; do
     end_i=$((start_i + n_mols))
     echo "start_i: $start_i, end_i: $end_i"
     sbatch run_scattering_inv_wavelet.sh --export=start_i
+    start_i=$end_i
 done
