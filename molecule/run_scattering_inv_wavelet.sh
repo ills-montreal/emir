476d011cc9097e07e2eeb43d65eb59a8600cbd65
Fransou <philippe.form@gmail.com>
2024-02-27 10:17:33 -0500

    [mol|cluster] Add scripts to compute scattering wavelets on comp-canada

---
+++ molecule/run_scattering_inv_wavelet.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=precompute-3d
+#SBATCH --account=def-ibenayed
+#SBATCH --time=0-06:00:00
+#SBATCH --mem=128G
+#SBATCH --cpus-per-task=8
+#SBATCH --tasks-per-node=1
+#SBATCH --nodes=1
+#SBATCH --output=%x-%j.out
+
+cd $SLURM_TMPDIR
+mkdir tmp_dir
+cd tmp_dir
+
+cp -r /home/fransou/scratch/emir/emir .
+cd emir/molecule
+
+module load python/3.10
+module load scipy-stack
+
+source /home/fransou/EMIR/bin/activate
+export WANDB_MODE=offline
+
+cp -r /home/fransou/scratch/DATA/EMIR/data/* data
+cp -r /home/fransou/scratch/DATA/EMIR/backbone_pretrained_models/* backbone_pretrained_models
+
+
+python utils/scattering_wavelet.py --dataset ClinTox--i-start $start_i
