69b2df46f0f6ab6a322687cd94d9d3e8f78e21dc
Fransou <philippe.form@gmail.com>
2024-03-01 09:49:43 -0500

    [mol|cluster] Modify mds_run.sh to get idea of time cplx

--- molecule/bash_scripts/mds_run.sh
+++ molecule/bash_scripts/mds_run.sh
@@ -8,15 +8,19 @@
 #SBATCH --nodes=1
 #SBATCH --output=%x-%j.out
 
+export DATASET="ZINC"
+export FP_LENGTH=1024
+export OUT_DIM=64
+
 echo "Running script"
 cd $SLURM_TMPDIR
 mkdir tmp_dir
 cd tmp_dir
 
 echo "Copying files"
-cp -r /home/fransou/scratch/emir/emir .
+cp -r /home/fransou/scratch/emir .
 cd emir/molecule
-cp -r /home/fransou/scratch/DATA/EMIR/data/ZINC data
+cp -r /home/fransou/scratch/DATA/EMIR/data/$DATASET data
 
 echo "Loading modules"
 module load python/3.10
@@ -24,4 +28,4 @@ module load scipy-stack
 source /home/fransou/EMIR/bin/activate
 
 echo "Running script"
-python make_fingerprint_continuous.py --dataset ZINC --fp-length 1024 --out-dim 64 --out-dir /home/fransou/scratch/DATA/EMIR/data/fingerprint_continuous
+python make_fingerprint_continuous.py --dataset $DATASET --fp-length $FP_LENGTH --out-dim $OUT_DIM --descriptors cats --out-dir /home/fransou/scratch/DATA/EMIR/data/fingerprint_continuous
