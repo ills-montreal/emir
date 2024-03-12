33a03ab226a40cf4612d1242a72503421cce76ec
Icannos <astaligorn@live.com>
2024-03-12 16:28:30 -0400

    fix kinfe estimator

--- nlp_embeddings/scripts/generate_embeddings.py
+++ nlp_embeddings/scripts/generate_embeddings.py
@@ -1,6 +1,12 @@
-from emb_datasets import load_emb_dataset, AVAILABLE_DATASETS
+from emb_datasets import (
+    load_emb_dataset,
+    AVAILABLE_DATASETS,
+    TASKS_DATASET,
+    load_emd_classif_dataset,
+)
 import argparse
 import torch
+import numpy as np
 
 from pathlib import Path
 from cache_models import SMALL_MODELS, LARGE_MODELS
@@ -11,10 +17,10 @@ from sentence_transformers import SentenceTransformer
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, required=True)
+    parser.add_argument("--classification_task", action="store_true", default=False)
     parser.add_argument(
         "--split",
         type=str,
-        required=True,
         choices=["train", "validation", "test"],
         default="test",
     )
@@ -30,25 +36,48 @@ def parse_args():
 
 def main():
     args = parse_args()
-    dataset = load_emb_dataset(
-        args.dataset, AVAILABLE_DATASETS[args.dataset], args.split
-    )
 
-    model = SentenceTransformer(args.model)
+    if not args.classification_task:
+        dataset = load_emb_dataset(
+            args.dataset, AVAILABLE_DATASETS[args.dataset], args.split
+        )
 
-    output_dir = Path(args.output_dir) / args.model / args.dataset / args.split
-    output_dir.mkdir(parents=True, exist_ok=True)
+        model = SentenceTransformer(args.model)
 
-    embeddings = model.encode(
-        dataset["text"],
-        batch_size=args.batch_size,
-        show_progress_bar=True,
-        convert_to_numpy=True,
-        device=args.device,
-    )
+        output_dir = Path(args.output_dir) / args.model / args.dataset / args.split
+        output_dir.mkdir(parents=True, exist_ok=True)
 
-    with open(output_dir / "embeddings.npy", "wb") as f:
-        torch.save(embeddings, f)
+        embeddings = model.encode(
+            dataset["text"],
+            batch_size=args.batch_size,
+            show_progress_bar=True,
+            convert_to_numpy=True,
+            device=args.device,
+        )
+
+        with open(output_dir / "embeddings.npy", "wb") as f:
+            torch.save(embeddings, f)
+
+    else:
+        dataset, splits, metadata = load_emd_classif_dataset(args.dataset)
+
+        model = SentenceTransformer(args.model)
+
+        for split in splits:
+            _output_dir = Path(args.output_dir) / args.model / args.dataset / split
+            _output_dir.mkdir(parents=True, exist_ok=True)
+            embeddings = model.encode(
+                dataset[split]["text"],
+                batch_size=args.batch_size,
+                show_progress_bar=True,
+                convert_to_numpy=True,
+                device=args.device,
+            )
+            with open(_output_dir / "embeddings.npy", "wb") as f:
+                torch.save(embeddings, f)
+            # save the labels
+            with open(_output_dir / "labels.npy", "wb") as f:
+                torch.save(dataset[split]["label"], f)
 
 
 if __name__ == "__main__":
