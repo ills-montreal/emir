5e15d2e3be97d0aaf36943ec29b530b1e03548c4
Fransou <philippe.form@gmail.com>
2024-03-19 15:03:22 -0400

    [mol|slurm] Submit emir bash file

--- molecule/bash_scripts/emir_mol_MI_estimation.sh
+++ molecule/bash_scripts/emir_mol_MI_estimation.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=def-ibenayed
 #SBATCH --time=0-03:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --mem=30G
+#SBATCH --mem=40G
 #SBATCH --cpus-per-task=4
 #SBATCH --tasks-per-node=1
 #SBATCH --nodes=1
